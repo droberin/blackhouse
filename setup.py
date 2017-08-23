e0e9195fa694bf3de67c487b56f01c33344e7508
Roberto Salgado <drober+android@gmail.com>
2017-08-23 17:43:15 +0200

    Fixed source URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='blackhouse',
     version='0.1.0',
     packages=['blackhouse'],
-    url='https://github.com/drober/blackhouse',
+    url='https://github.com/droberin/blackhouse',
     license='MIT',
     author='Roberto Salgado',
     author_email='drober@gmail.com',
