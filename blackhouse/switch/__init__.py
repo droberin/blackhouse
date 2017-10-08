931b4f88abde2181e76b376688913cb329d47aa9
Roberto Salgado <drober+android@gmail.com>
2017-10-08 19:46:53 +0200

    Fixed Proper IMPORT! DAMN IT!

--- blackhouse/switch/__init__.py
+++ blackhouse/switch/__init__.py
@@ -20,7 +20,7 @@ blackhouse_service_type = getenv('BH_SERVICE_TYPE', 'controller')
 print("Local device is running as: {}".format(blackhouse_service_type))
 if blackhouse_service_type == 'push':
     print("Device is PUSH. Trying to load GPIO.")
-    from RPi.GPIO import GPIO
+    import RPi.GPIO as GPIO
 
 
 def check_auth(username, password):
